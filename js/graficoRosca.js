34a036c539cc8dcc1475e02d6a334a0579becfed
HisaeNakagima <1823289@aluno.univesp.br>
2023-08-14 12:19:30 -0300

    Criação do gráfico de rosca

---
+++ js/graficoRosca.js
@@ -0,0 +1,29 @@
+google.charts.load("current", { packages: ["corechart"] });
+
+function desenharRosca() {
+  var tabela = new google.visualization.DataTable();
+  tabela.addColumn("string", "categorias");
+  tabela.addColumn("number", "valores");
+  tabela.addRows([
+    ["Educação", 2000],
+    ["Transporte", 500],
+    ["Lazer", 230],
+    ["Saúde", 50],
+    ["Cartão de crédito", 900],
+    ["Alimentação", 260],
+  ]);
+
+  var opcoes = {
+    title: "Tipos de Gastos",
+    height: 300,
+    width: 400,
+    pieHole: 0.4,
+  };
+
+  var grafico = new google.visualization.PieChart(
+    document.getElementById("graficoRosca")
+  );
+  grafico.draw(tabela, opcoes);
+}
+
+google.charts.setOnLoadCallback(desenharRosca);
