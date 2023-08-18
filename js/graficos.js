61fd9b029bc8da9f038eb8203744b5717c1a689b
HisaeNakagima <1823289@aluno.univesp.br>
2023-08-18 15:42:53 -0300

    Criação do gráfco de barras

--- js/graficos.js
+++ js/graficos.js
@@ -132,6 +132,42 @@ function desenharGraficos() {
     document.getElementById("graficoColunaSurpresa")
   );
   grafico.draw(tabela, opcoes);
+
+  //grafico de barras
+  var tabela = new google.visualization.DataTable();
+  tabela.addColumn("string", "categorias");
+  tabela.addColumn("number", "valores");
+  tabela.addColumn({ type: "string", role: "annotation" });
+  tabela.addColumn({ type: "string", role: "style" });
+  tabela.addRows([
+    ["Educação", 2000, "R$2.000,00", "blue"],
+    ["Transporte", 500, "R$500,00", "grey"],
+    ["Lazer", 230, "R$230,00", "grey"],
+    ["Saúde", 50, "R$50,00", "grey"],
+    ["Cartão de crédito", 900, "R$900,00", "#8904B1"],
+    ["Alimentação", 260, "R$260,00", "grey"],
+  ]);
+
+  tabela.sort([{ column: 1, desc: true }]);
+
+  var opcoes = {
+    title: "Tipos de Gastos",
+    height: 400,
+    width: 800,
+    vAxis: { gridlines: { cout: 0 } },
+    legend: "none",
+    hAxis: {
+      gridlines: { color: "transparent" },
+      format: "currency",
+      textPosition: "none",
+    },
+    annotations: { alwaysOutside: true },
+  };
+
+  var grafico = new google.visualization.BarChart(
+    document.getElementById("graficoBarra")
+  );
+  grafico.draw(tabela, opcoes);
 }
 
 google.charts.setOnLoadCallback(desenharGraficos);
